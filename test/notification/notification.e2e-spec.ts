007eced214960260878e0719f5a58def074649cd
valentinov8060 <valentinovbill0@gmail.com>
2025-05-06 21:22:19 +0700

    feat(notification): Add E2E tests

---
+++ test/notification/notification.e2e-spec.ts
@@ -0,0 +1,77 @@
+import { join } from 'path';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as request from 'supertest';
+import helmet from 'helmet';
+import { AppModule } from 'src/app.module';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+
+describe('NotificationController (e2e)', () => {
+  let app: NestExpressApplication;
+  const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI4NjVkMDhhLTEyNmMtNDQ4Mi05YTU2LTBkY2Q0ODQyMWY2MyIsInJvbGUiOiJSZWd1bGFyIFVzZXIiLCJpYXQiOjE3NDY1MDEzNTR9.2N8RHoejnxr6JI1c9SQhQm2oEl8mYuu6fuQCjVptTo4';
+
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    // Middleware for security
+    app.use(helmet());
+    // Pipes configuration
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // Only allow properties that are defined in the DTO
+        forbidNonWhitelisted: true, // Reject requests with non-whitelisted properties
+        transform: true, // Transform payloads to DTO instances
+      }),
+    );
+    // Filter configuration
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    app.setBaseViewsDir(join(__dirname, '..', '..', 'src', 'common', 'views'));
+    app.setViewEngine('hbs');
+
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  // Get notification
+  it('successfully notification', async () => {
+    const res = await request(app.getHttpServer())
+      .get(`/notification`)
+      .set('Authorization', `Bearer ${userToken}`)
+
+    console.log('successfully notification response:', res.body);
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+  });
+
+  // Delete specific notification
+  it('successfully delete specific notification', async () => {
+    const res = await request(app.getHttpServer())
+      .delete(`/notification/a392c71a-5275-42c4-bcde-dacfa9940864`)
+      .set('Authorization', `Bearer ${userToken}`)
+
+    console.log('successfully delete specific notification response:', res.body);
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+  });
+
+  // Delete all notification
+  it('successfully delete all notification', async () => {
+    const res = await request(app.getHttpServer())
+      .delete(`/notification`)
+      .set('Authorization', `Bearer ${userToken}`)
+
+    console.log('successfully delete all notification response:', res.body);
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+  });
+});
